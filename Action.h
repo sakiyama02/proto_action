546714cbdcbb016f4709d87724fb132c7475976c
sakiyama <sakiyama.hayato02@gmail.com>
2022-07-21 15:15:16 +0900

    create

---
+++ Action.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "./system.h"
+
+/// 2022/07/13
+/// yoshitome
+/// Action.h
+
+class Action {
+    private:
+
+    public:
+        Action();
+        virtual ~Action();
+
+        virtual int8_t run(int32_t speed) = 0;
+
+};
